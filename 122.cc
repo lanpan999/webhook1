6c0ca6484ee79c50c53a974ece253bf784e821ab
lanpan999 <41445050+lanpan999@users.noreply.github.com>
2020-10-22 15:31:07 +0800

    Create 122.cc

---
+++ 122.cc
@@ -0,0 +1,18 @@
+ datasource:
+    druid:
+      dangdang:
+        driver-class-name: com.mysql.jdbc.Driver
+        url: jdbc:mysql://102.168.0.14:13306/dangdang?characterEncoding=utf-8&useSSL=false
+        username: dataway-rw
+        password: QqHVMhmN*8
+      new-vip:
+        driver-class-name: com.mysql.jdbc.Driver
+        url: jdbc:mysql://172.168.0.14:13306/npkw_vip?characterEncoding=utf-8&useSSL=false
+        username: dataway-rw
+        password: QqHVMhmN*8
+      huazhu:
+        driver-class-name: com.mysql.jdbc.Driver
+        url: jdbc:mysql://192.168.0.14:13306/huazhu?characterEncoding=utf-8&useSSL=false
+        username: dataway-rw
+        password: QqHVMhmN*8
+        huazhu.com
